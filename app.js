9181b0038f785e5869b02c2ddb023fbc17f78696
Jamiel Rahi <jamiel.nr@gmail.com>
2019-12-04 22:15:03 -0500

    feat: some extra responses

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-require('dotenv').config()
-const TelegramBot = require('node-telegram-bot-api');
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.API_KEY;
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
-  console.log(msg)
+bot.on("message", msg => {
+  console.log(msg);
 
   const reply = text => bot.sendMessage(msg.chat.id, text);
-  const josh = () => bot.sendSticker(msg.chat.id, JOSH_STICKER)
+  const josh = () => bot.sendSticker(msg.chat.id, JOSH_STICKER);
+  const linux = () => bot.sendSticker(msg.chat.id, randomLinux());
+  const goodBot = () => bot.sendSticker(msg.chat.id, GOOD_BOT);
 
-  if (msg.from.username === 'Vashmata') reply('JOSH HAS SPOKEN.')
-  else if (/.*josh.*/i.test(msg.text)) josh()
-  else if (msg.new_chat_members) reply(WELCOME_MESSAGE(msg.new_chat_members))
-  else if (/.*bot github.*/.test(msg.text)) reply(GITHUB_LINK)
-  else if (/.*bot.*/i.test(msg.text)) reply("Did someone say bot?")
-
+  if (msg.from.username === "Vashmata") reply("JOSH HAS SPOKEN.");
+  else if (/.*josh.*/i.test(msg.text)) josh();
+  else if (/.*linux.*/.test(msg.text)) reply(GITHUB_LINK);
+  else if (msg.new_chat_members) reply(WELCOME_MESSAGE(msg.new_chat_members));
+  else if (/.*linux.*/.test(msg.text)) linux();
+  else if (/.*bad bot.*/.test(msg.text)) reply("Fuck you");
+  else if (/.*good bot.*/.test(msg.text)) goodBot();
+  else if (/.*bot github.*/.test(msg.text)) reply(GITHUB_LINK);
+  else if (/.*bot.*/i.test(msg.text)) reply("Did someone say bot?");
 });
 
-const JOSH_STICKER = 'CAADAQADbgADmY5hL18k-jQuCglHFgQ'
-const GITHUB_LINK = 'https://github.com/closetothe/banter-bot'
-const WELCOME_MESSAGE = users => `Welcome to the linux shitposting group: ${users.map(u => u.first_name).join(', ')}.
-It is meant for linux talk but often ends up being just memes or @jamielnr and Stuart talking about random ass shit for 400 messages`
+const randomLinux = () => {
+  const i = Math.floor(Math.random() * 10) % 4;
+  return LINUX_STICKERS[i];
+};
+
+const LINUX_STICKERS = [
+  "CAADBAADZAEAApdrhgTNh414jbPDihYE",
+  "CAADBAAD_QADl2uGBLaZ6SiHh0w4FgQ",
+  "CAADBAADjwEAApdrhgThTIzm5zjdpxYE",
+  "CAADBAADGwEAApdrhgRugj8xK_OZzhYE"
+];
+const GOOD_BOT = "CAADAQADMAADmY5hL3UAAUlp0ev2xhYE";
+const JOSH_STICKER = "CAADAQADbgADmY5hL18k-jQuCglHFgQ";
+const GITHUB_LINK = "https://github.com/closetothe/banter-bot";
+const WELCOME_MESSAGE = users => `Welcome to the linux shitposting group: ${users
+  .map(u => u.first_name)
+  .join(", ")}.
+It is meant for linux talk but often ends up being just memes or @jamielnr and Stuart talking about random ass shit for 400 messages`;
