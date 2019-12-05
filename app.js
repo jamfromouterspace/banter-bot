f6f80eaada900562178336d94d820b111f1c40f4
closetothe <jamiel.nr@gmail.com>
2019-12-05 02:11:40 +0000

    fix: switch regex to case-insensitive

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ const token = process.env.API_KEY;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
-  const reply = text => bot.sendMessage(msg.chat.id, text);
   console.log(msg)
+
+  const reply = text => bot.sendMessage(msg.chat.id, text);
+  const josh = () => bot.sendSticker(msg.chat.id, JOSH_STICKER)
+
   if (msg.from.username === 'Vashmata') reply('JOSH HAS SPOKEN.')
+  else if (/.*josh.*/i.test(msg.text)) josh()
   else if (msg.new_chat_members) reply(WELCOME_MESSAGE(msg.new_chat_members))
-  else if (/.*bot.*/.test(msg.text)) reply("Did someone say bot?")
+  else if (/.*bot.*/i.test(msg.text)) reply("Did someone say bot?")
+
 });
 
-
+const JOSH_STICKER = 'CAADAQADbgADmY5hL18k-jQuCglHFgQ'
 const WELCOME_MESSAGE = users => `Welcome to the linux shitposting group: ${users.map(u => u.first_name).join(', ')}.
 It is meant for linux talk but often ends up being just memes or @jamielnr and Stuart talking about random ass shit for 400 messages`
