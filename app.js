2a163004dd985167add19f8e270d328d533809fb
closetothe <jamiel.nr@gmail.com>
2019-12-05 01:57:57 +0000

    feat: send welcome message to new members

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
   const reply = text => bot.sendMessage(msg.chat.id, text);
+  console.log(msg)
   if (msg.from.username === 'Vashmata') reply('JOSH HAS SPOKEN.')
-  else if (/.*bot.*/.test(msg.text.toString())) reply("Did someone say bot?")
+  else if (msg.new_chat_members) reply(WELCOME_MESSAGE(msg.new_chat_members))
+  else if (/.*bot.*/.test(msg.text)) reply("Did someone say bot?")
 });
+
+
+const WELCOME_MESSAGE = users => `Welcome to the linux shitposting group: ${users.map(u => u.first_name).join(', ')}.
+It is meant for linux talk but often ends up being just memes or @jamielnr and Stuart talking about random ass shit for 400 messages`
